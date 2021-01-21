ec8dab4058958a9c50c4d79318af819024f43cb6
levtrilev <levtrishankov@yandex.ru>
2021-01-21 20:03:48 +0300

    Base framework and layout template completed

---
+++ src/components/excel/Excel.js
@@ -0,0 +1,43 @@
+import { $ } from '../../core/Dom'
+
+export class Excel {
+    constructor(selector, options) {
+        // this.$el = document.querySelector(selector)
+        this.$el = $(selector)
+        this.components = options.components || []
+    }
+
+    getRoot() {
+        const $root = $.create('div', 'excel')
+        // const $root = document.createElement('div')
+        // $root.classList.add('excel')
+
+        this.components = this.components.map(Component => {
+            const $el = $.create('div', Component.className)
+            // const $el = document.createElement('div')
+            // $el.classList.add(Component.className)
+            const component = new Component($el)
+            // $el.innerHTML = component.toHTML()
+            // DEBUG
+            if (component.name) {
+                window['c_' + component.name] = component
+            }
+            $el.html(component.toHTML())
+            // $root.insertAdjacentHTML('beforeend', component.toHTML())
+            $root.append($el)
+            return component
+        });
+        return $root
+    }
+
+    render() {
+        // afterbegin, afterend, beforebegin, beforeend
+        // this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`)
+        // const node = document.createElement('h1')
+        // node.textContent = 'more TEST'
+        // this.$el.append(node)
+        this.$el.append(this.getRoot())
+        // console.log(this.components)
+        this.components.forEach(component => component.init());
+    }
+}
---
+++ src/components/table/table.template.js
@@ -0,0 +1,53 @@
+import { stringify } from 'querystring'
+
+const CODES = {
+    A: 65,
+    Z: 90
+}
+
+function toCell(selected = '', _) {
+    return `
+        <div class="cell${selected}" contenteditable>
+        </div>
+    `
+}
+
+function toColumn(char = 'A') {
+    return `
+    <div class="column">
+        ${char}
+    </div>
+    `
+}
+
+function createRow(index, content) {
+    return `
+        <div class="row">
+            <div class="row-info">${index !== null ? index : ''}</div>
+            <div class="row-data">${content}</div>
+        </div>
+    `
+}
+
+function toChar(_, index) {
+    return String.fromCharCode(index + 65)
+}
+
+export function createTable(rowsCount = 10) {
+    const colsCount = CODES.Z - CODES.A + 1
+    const rows = []
+    const cols = new Array(colsCount)
+        .fill('')
+        .map(toChar)
+        .map(toColumn)
+        .join('')
+    rows.push(createRow(null, cols))
+    const cells = new Array(colsCount)
+        .fill('')
+        .map(toCell)
+        .join('')
+    for (let i = 0; i < rowsCount; i++) {
+            rows.push(createRow(i + 1, cells))
+    }
+    return rows.join('')
+}
